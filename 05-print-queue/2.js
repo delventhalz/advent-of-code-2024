c5e7f9ba8c4d546ff2258e652d1d6be6105a473d
Zac Delventhal <delventhalz@gmail.com>
2024-12-05 21:38:31 -0600

    Solve 2024 Day 5: Print Queue
    I am neither happy nor unhappy with this solution. It worked. Feels like
    it could be better, but I see no obvious improvements to make right now.
    
    ✩ 00:24:01 (1) 5065
    ★ 00:28:03 (2) 2768

---
+++ 05-print-queue/2.js
@@ -0,0 +1,50 @@
+/**
+ * --- Advent of Code 2024 ---
+ *
+ * Day 5: Print Queue
+ * (Part 2)
+ *
+ * https://adventofcode.com/2024/day/5#part2
+ */
+
+import { groupBy, mapValues } from 'lodash-es';
+import { sum } from '../lib/index.js';
+
+const isOrdered = (rulebook, update) => {
+  for (let i = 1; i < update.length; i += 1) {
+    const page = update[i];
+    const mustFollows = rulebook[page] ?? [];
+
+    for (const mustFollow of mustFollows) {
+      const followIndex = update.indexOf(mustFollow);
+      if (followIndex !== -1 && followIndex < i) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+export default function main({ input }) {
+  const [rulesString, updatesString] = input.split('\n\n');
+  const rules = rulesString.split('\n').map(str => str.split('|').map(Number));
+  const updates = updatesString.split('\n').map(str => str.split(',').map(Number));
+
+  const rulebook = mapValues(
+    groupBy(rules, pair => pair[0]),
+    rules => rules.map(pair => pair[1])
+  );
+
+  const unorderedUpdates = updates.filter(update => !isOrdered(rulebook, update));
+
+  for (const update of unorderedUpdates) {
+    update.sort((a, b) => {
+      const mustFollows = rulebook[a] ?? [];
+      return mustFollows.includes(b) ? -1 : 1;
+    });
+  }
+
+  const middlePages = unorderedUpdates.map(update => update[Math.floor(update.length / 2)]);
+  return sum(middlePages);
+}
