d7c57cf1b09a6f2a0c80d1a254c697e69d79048e
Zac Delventhal <delventhalz@gmail.com>
2024-12-09 23:32:51 -0600

    Solve 2024 Day 10: Hoof It
    I'm just starting with a stack and a while loop instead of recursive
    solutions these days. It's a shame because I like recursion, but the
    cost of refactoring once I discover I've blown the stack is just too
    high.
    
    ✩ 00:22:31 (2) 3512
    ★ 00:30:03 (3) 3759

---
+++ 10-hoof-it/2.js
@@ -0,0 +1,47 @@
+/**
+ * --- Advent of Code 2024 ---
+ *
+ * Day 10: Hoof It
+ * (Part 2)
+ *
+ * https://adventofcode.com/2024/day/10#part2
+ */
+
+import { eachAdjacent, eachMatrix, sum } from '../lib/index.js';
+
+const scoreTrailhead = (matrix, trailhead) => {
+  const peaks = [];
+  const stack = [trailhead];
+
+  while (stack.length > 0) {
+    const [x, y] = stack.pop();
+    const currentHeight = matrix[y][x];
+
+    eachAdjacent(matrix, [x, y], (height, coords) => {
+      if (height - currentHeight === 1) {
+        if (height === 9) {
+          peaks.push(coords);
+        } else {
+          stack.push(coords);
+        }
+      }
+    });
+  }
+
+  return peaks.length;
+};
+
+export default function main({ matrix }) {
+  eachMatrix(matrix, (char, [x, y]) => {
+    matrix[y][x] = Number(char)
+  });
+
+  const trailheads = [];
+  eachMatrix(matrix, (height, coords) => {
+    if (height === 0) {
+      trailheads.push(coords);
+    }
+  });
+
+  return sum(trailheads.map(trailhead => scoreTrailhead(matrix, trailhead)));
+}
